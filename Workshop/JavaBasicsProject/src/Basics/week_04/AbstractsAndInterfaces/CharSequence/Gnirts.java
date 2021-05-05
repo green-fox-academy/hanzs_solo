2b1034b777e1a2bd1e5547f626dc6c8a9385767e
Hanzs Gergely <hanzsgergely@gmail.com>
2021-05-05 11:05:43 +0200

    update

--- Workshop/JavaBasicsProject/src/Basics/week_04/AbstractsAndInterfaces/CharSequence/Gnirts.java
+++ Workshop/JavaBasicsProject/src/Basics/week_04/AbstractsAndInterfaces/CharSequence/Gnirts.java
@@ -1,15 +1,17 @@
 package Basics.week_04.AbstractsAndInterfaces.CharSequence;
 
-//-Create a Gnirts class implementing this interface
-//    -It should have one String field that can be set via the constructor
-//    -Your solution should be a class that represents a reversed String. Implement its methods accordingly. E.g. if the Gnirts is constructed with the String "example" then the character at the 2nd index is 'p'
-//    -In a main method try out all the methods
 
+/**
+ * -Create a Gnirts class implementing this interface
+ * -It should have one String field that can be set via the constructor
+ * -Your solution should be a class that represents a reversed String. Implement its methods accordingly. E.g. if the Gnirts is constructed with the String "example" then the character at the 2nd index is 'p'
+ * -In a main method try out all the methods
+ */
 public class Gnirts implements CharSequence {
 
   //
   //fields
-  private String text;
+  private final String text;
 
   //
   //constructors
--- Workshop/JavaBasicsProject/src/Basics/week_04/AbstractsAndInterfaces/CharSequence/ShiftedCharSequence.java
+++ Workshop/JavaBasicsProject/src/Basics/week_04/AbstractsAndInterfaces/CharSequence/ShiftedCharSequence.java
@@ -1,15 +1,18 @@
 package Basics.week_04.AbstractsAndInterfaces.CharSequence;
 
-                    //-Create a ShiftedCharSequence class implementing this interface
+/**
+ * -Create a ShiftedCharSequence class implementing this interface
+ * -This CharSequence can be constructed using the following arguments:
+ * -a String (that is to be shifted)
+ * -a number (which defines how many characters to shift)
+ * -Implement the methods so that the sequence is shifted by the defined number of characters
+ */
+
 public class ShiftedCharSequence implements CharSequence {
   //
   // fields
-  private String text;
+  private final String text;
 
-                    //  -This CharSequence can be constructed using the following arguments:
-                    //      -a String (that is to be shifted)
-                    //      -a number (which defines how many characters to shift)
-                    //  -Implement the methods so that the sequence is shifted by the defined number of characters
   //
   // constructors
   public ShiftedCharSequence(String text, int number) {
